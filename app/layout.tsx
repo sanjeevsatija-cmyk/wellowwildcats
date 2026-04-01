34cd5ff9fa456ac824800a4271afa33ebba4742a
Sanjeev <sanjeevsatija@gmail.com>
2026-04-01 11:34:09 +1000

    Font change: DM Serif Display + Raleway

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,7 +83,7 @@ function ComingSoon() {
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    if (process.env.COMING_SOON === "true") {
+  if (process.env.COMING_SOON === "true") {
     return <ComingSoon />;
   }
 
@@ -94,7 +94,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,700;0,900;1,700;1,900&family=Barlow:wght@300;400;500;600;700&family=Barlow+Condensed:wght@600;700;800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Raleway:wght@600;700;800&family=Barlow:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
